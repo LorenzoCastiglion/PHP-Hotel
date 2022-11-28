835316d4476f94c136334b862a611f7f64848ccb
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-28 16:39:13 +0100

    bonus 1 completo

--- index.php
+++ index.php
@@ -39,7 +39,22 @@ $hotels = [
 
 ];
 
-$filterArr = [];
+
+
+if (isset($_GET['parking']) && !empty($_GET['parking'])) {
+    $filterArr = [];
+
+    foreach ($hotels as $hotel) {
+        if ($hotel['parking'] && $_GET['parking'] == 'parking') {
+
+            $filterArr[] = $hotel;
+
+        } else if (!$hotel['parking'] && $_GET['parking'] == 'no-parking') {
+            $filterArr[] = $hotel;
+        }
+    }
+    $hotels = $filterArr;
+}
 
 
 
@@ -62,55 +77,60 @@ $filterArr = [];
 </head>
 
 <body>
+    <div class="wrapper position-absolute">
 
-    <div class="col-6 m-auto">
-        <select name="type" id="type" class="form-select" aria-label="Default select example">
-            <option selected>Scegli</option>
-            <option value="parking">parking</option>
-            <option value="vote">vote</option>
-        </select>
-        <button type="submit" class="btn btn-warning text-center mt-3 col-12 ">Search</button>
+        <div>
+            <form action="index.php" method="GET" name="formFilter" class="col-6 m-auto">
+                <select name="parking" id="parking" class="form-select" aria-label="Default select example">
+                    <option selected>Scegli</option>
+                    <option value="parking">parking</option>
+                    <option value="no-parking">no-parking</option>
+                </select>
+                <button type="submit" class="btn btn-warning text-center mt-3 col-6 ">Search</button> <button
+                    type="submit" class="btn btn-danger text-center mt-3 col-4 ">Reset</button>
+            </form>
+        </div>
+
+        <table class="px-4 mt-5 table table-dark table-hover">
+            <thead>
+                <tr class="  text-uppercase title">
+                    <th scope="col">name</th>
+                    <th scope="col">description</th>
+                    <th class="text-center" scope="col">parking</th>
+                    <th class="text-center" scope="col">vote</th>
+                    <th class="text-center" scope="col">distance from center</th>
+                </tr>
+            </thead>
+
+
+            <?php foreach ($hotels as $hotel) {
+                $parcheggio = $hotel['parking'] ? 'si' : 'no';
+            ?>
+
+            <tbody class="  descr">
+                <tr>
+                    <td>
+                        <?php echo $hotel['name'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['description'] ?>
+                    </td>
+                    <td class=" text-uppercase text-center">
+                        <?php echo $parcheggio ?>
+                    </td>
+                    <td class=" text-center">
+                        <?php echo $hotel['vote'] ?>
+                    </td>
+                    <td class="  text-center">
+                        <?php echo $hotel['distance_to_center'] ?> Km
+                    </td>
+                </tr>
+            </tbody>
+
+            <?php } ?>
+
+
     </div>
-
-    <table class=" mt-5 table table-dark table-hover">
-        <thead>
-            <tr class=" text-center text-uppercase">
-                <th scope="col">name</th>
-                <th scope="col">description</th>
-                <th scope="col">parking</th>
-                <th scope="col">vote</th>
-                <th scope="col">distance from center</th>
-            </tr>
-        </thead>
-
-
-        <?php foreach ($hotels as $hotel) {
-            $parcheggio = $hotel['parking'] ? 'si' : 'no';
-        ?>
-
-        <tbody class=" text-center">
-            <tr>
-                <td>
-                    <?php echo $hotel['name'] ?>
-                </td>
-                <td>
-                    <?php echo $hotel['description'] ?>
-                </td>
-                <td class=" text-uppercase ">
-                    <?php echo $parcheggio ?>
-                </td>
-                <td>
-                    <?php echo $hotel['vote'] ?>
-                </td>
-                <td>
-                    <?php echo $hotel['distance_to_center'] ?> Km
-                </td>
-            </tr>
-        </tbody>
-
-        <?php } ?>
-
-
 
 
 
